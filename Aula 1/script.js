ac738b2d42a8aec626b68bdbe0086bcb8c2d6e59
Dário <dariojr1233@gmail.com>
2023-03-20 19:32:47 -0300

    Aula - Git - Kaspper Academy

---
+++ Aula 1/script.js
@@ -0,0 +1,15 @@
+
+
+mudarTanj = () => {
+    var tanj = document.getElementById('tanjiro')
+    if(tanj.className == 'tanj1'){
+        tanj.className = 'tanj2'
+    }else{
+        tanj.className = 'tanj1'
+    }
+        
+    
+    
+}
+
+
