d41bcfbd07d1ac10c9d6beeb3cbf305fc5ef8aee
Dário <dariojr1233@gmail.com>
2023-03-22 22:06:12 -0300

    Aula 1 Js - Segunda edição kaspper Academy

---
+++ Aula 12 - JavaScript/script.js
@@ -0,0 +1,40 @@
+divC = document.querySelector('.container')
+divC.addEventListener('mousemove',bolinhaMoves)
+
+
+
+if(true){
+    var minhaVariavel = 'teste';
+    console.log('Dentro do If '+ minhaVariavel)
+}
+
+if(true){
+    let minhaVariavel = 'TesteL'
+    
+    console.log('Dentro da minha Variável if '+ minhaVariavel);
+}
+
+
+/* Variáveis */
+
+console.log('-----------');
+var variavelString = 'minha string'
+var numerica = 2023
+var boolean = false
+var undefined
+
+/*  */
+function bolinhaMoves() {
+    console.log(divC.style);
+    console.log(window);
+} 
+
+
+
+
+
+
+
+
+
+
