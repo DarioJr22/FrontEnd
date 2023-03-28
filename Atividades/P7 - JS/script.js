9eecc9a5cd5be27e33c88e4843ee44691b86e055
Dário <dariojr1233@gmail.com>
2023-03-28 19:30:12 -0300

    Atividade - Soma de produtos em JS

--- Atividades/P7 - JS/script.js
+++ Atividades/P7 - JS/script.js
@@ -1,9 +1,22 @@
 //Atividade Js
+//Somar textos
+ function somarTexto(){
+    let resultadoStr =  document.querySelector('.resultadoString')
+    let inputStrings =  document.querySelectorAll('.somaString')
+    resultadoStr.value = inputStrings[0].value + inputStrings[1].value
+ }
 
- var inputStrings =  document.querySelectorAll('.somaString')
- var resultadoStr = inputStrings[0].value +  inputStrings[0].value  
+ function somarNumeros(){
+    let resultadoNumb =  document.querySelector('.resultadoNumer')
+    let inputNumbers =  document.querySelectorAll('.somaNumber')
+    resultadoNumb.value = eval( inputNumbers[0].value + inputNumbers[1].value)
+ }
+
+ function parOuImpar(params) {
+    let parOuImpar =  document.querySelector('.Par')
+    parOuImpar.value % 2 == 0 ? alert('É PAR MACHADO') : alert('É IMPAR MACHADO')
+ }
 
 
- var inputStrings =  document.querySelectorAll('.somaNumber')
- var resultadoStr = eval(inputStrings[0].value +  inputStrings[0].value)  
+
 
