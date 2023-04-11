bbd8df2d4e07791491db5f42f39dce73e90ed7d3
Dário <dariojr1233@gmail.com>
2023-04-10 21:24:57 -0300

    Aula Funcoes

---
+++ Aula 18 - FuncoesJS/funcoes.js
@@ -0,0 +1,13 @@
+function ola(){
+    console.log('Meu fi gosta de farinha ?');
+    console.log('Tu é dooooooooooooooooooooooooooooooooooooooooooooooooooooooid');
+}
+
+//ola();
+
+const olaMeufi = function (Meufi) {
+  return  Meufi =='gosta de farinha' ? 'tuéeeeeeeee dooooooooooooooooooooid' : 'mim dê papai'
+}
+
+console.log(olaMeufi('gosta de farinha'));
+
